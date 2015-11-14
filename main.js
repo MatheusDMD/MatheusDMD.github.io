034ff5e8daea0ca76a7b6b745adcf2cb0fb32b2c
matheusdmd <matheusdmd@al.insper.edu.br>
2015-11-14 13:00:55 -0200

    testando a outra tool

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function(){
+
+    $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 1100,
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el){
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+        }
+    })
+
+});
