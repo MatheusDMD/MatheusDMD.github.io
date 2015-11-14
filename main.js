fe9bcea23f466c414498c30649f2230e3756a00d
matheusdmd <matheusdmd@al.insper.edu.br>
2015-11-14 12:45:18 -0200

    testa2

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("#name").hover(function() {
     $("#name").css({
       "color": "red"
-      "font-size": "larger"
     });
   }, function() {
     $("#name").css({
