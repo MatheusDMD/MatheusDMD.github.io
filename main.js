bf7d8b431ea329ec7e058170fc79964927f81bfe
matheusdmd <matheusdmd@al.insper.edu.br>
2016-05-05 14:41:48 -0300

    renewed

--- main.js
+++ main.js
@@ -18,6 +18,13 @@ $(document).ready(function () {
         $(".grayscaleDS").removeClass().fadeTo(400, 0.8).addClass("grayscaleDSbw").fadeTo(400, 1);
     });
 
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    css3: true
+  });
+});
 
 
 $(document).ready(function(){
